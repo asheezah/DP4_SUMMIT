187608fe2bbb1eaea0d4e64dcd40ca293ab989ec
asheezah <aishazahid5@gmail.com>
2026-03-27 16:48:33 +0000

    Testing something

--- Home.py
+++ Home.py
@@ -1,11 +1,12 @@
 import streamlit as st
 
 st.set_page_config(initial_sidebar_state="expanded", page_title="Maccessible")
+"""
 st.markdown("""
     <style>
         [data-testid="stSidebarNav"] {display: none;}
     </style>
-""", unsafe_allow_html=True)
+""", unsafe_allow_html=True)"""
 
 
 
