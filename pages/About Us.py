fd48a92bdf95489a9221bfdd325e5114a110ba81
asheezah <aishazahid5@gmail.com>
2026-03-31 13:21:15 +0000

    About Us

--- pages/About Us.py
+++ pages/About Us.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
-st.markdown("It's all about us btw")
+st.title("Meet the Team")
+sep = st.col([3,1])
+sep[0] = st.markdown("""It's a :fire: team.
+                     We're in first year iBioMed @ Mac, and Maccesisble is our DP4 project that we built for Jany, a community member with Multiple Sclerosis, so that she could navigate buildings easier.
+                     Ummm I don't know what else to say... Pierce is the GOAT.""")
+sep[1] = st.image('AboutUs.jpeg', caption = "From left to right: Emily, Aisha, Erik, and Noah.")
+
+st.markdown("**If you want to contact us, go check out Dr McDonald's email.**")
