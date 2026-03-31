40abfe328fe0e58efee529a1eccf09b6e7608be6
asheezah <aishazahid5@gmail.com>
2026-03-31 13:53:55 +0000

    Making a commit with image.rotate() for iteration tracking purposes but I'm deleting that code and keep rotate.png after this commit. Also, streamlit puts jpeg images upside down, that's why I needed the rotate and convert to png

--- pages/About Us.py
+++ pages/About Us.py
@@ -1,10 +1,15 @@
 import streamlit as st
+from PIL import Image
+
+img = Image.open("pages/AboutUs.jpeg")
+rotate = img.rotate(180)
+rotate.save("pages/rotate.png")
 
 st.title("Meet the Team")
-sep = st.col([3,1])
-sep[0] = st.markdown("""It's a :fire: team.
+#sep = st.columns([3,2.7])
+st.markdown("""It's a :fire: team.
                      We're in first year iBioMed @ Mac, and Maccesisble is our DP4 project that we built for Jany, a community member with Multiple Sclerosis, so that she could navigate buildings easier.
                      Ummm I don't know what else to say... Pierce is the GOAT.""")
-sep[1] = st.image('AboutUs.jpeg', caption = "From left to right: Emily, Aisha, Erik, and Noah.")
+st.image('pages/rotate.png', caption = "From right to left: Noah, Erik, Aisha, and Emily.")
 
-st.markdown("**If you want to contact us, go check out Dr McDonald's email.**")
+st.markdown("**If you want to contact us, go check out Dr McDonald's contact info.**")
