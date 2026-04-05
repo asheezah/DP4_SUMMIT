561c86172a37b9f76c32251322a1d37e2f76e3f0
asheezah <aishazahid5@gmail.com>
2026-04-05 04:10:47 +0000

    I hate commits

--- pages/Map.py
+++ pages/Map.py
@@ -351,7 +351,7 @@ def find_closest_node(G, user_node_name): #pulls coordinates from nx graph, then
 
 def find_shortest_path(G, user_node_name, destination, heuristic, places): #returns shortest path out of specified type (ex. elevator)
     best_path = float('inf')
-    st.write(list(G.neighbors(user_node_name))
+    st.write(list(G.neighbors(user_node_name)))
     for des in places[destination]: #places['destination'] opens list of all ex. elevators, des = elevator1, etc
         target_name = des['name'].lower()
         possible_path = nx.astar_path_length(G, user_node_name, target_name, heuristic = heuristic, weight = 'weight')
